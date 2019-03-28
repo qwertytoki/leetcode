7c19e706321740bb085149f0951cd02f42396ccf
qwertytoki <qwertytoki@gmail.com>
2019-03-29 02:10:55 +0800

    https://leetcode.com/problems/reverse-integer/

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -1,5 +1,15 @@
 class Solution {
     public int reverse(int x) {
-        
+        boolean isMinus = false;
+        if(x<0){
+            isMinus = true;
+            x= x * -1;
+        }
+        String strX = String.valueOf(x);
+		StringBuffer sb = new StringBuffer(strX);
+		String dst = sb.reverse().toString();
+        int result = Integer.parseInt(dst);
+        if(isMinus) result = result*-1;
+        return result;
     }
 }
