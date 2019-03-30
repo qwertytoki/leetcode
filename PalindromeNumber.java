e043f12b22eb4459f1bb4459aaf0a8071bff3493
qwertytoki <qwertytoki@gmail.com>
2019-03-31 00:38:33 +0800

    done

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -1,5 +1,21 @@
+import java.util.List;
+
 class Solution {
     public boolean isPalindrome(int x) {
-        
-    }1
+        // 負の数の時点でNG
+        // 最後が0の時点でNG
+        if(x%10 == 0 || x<0) return false;
+        List<Integer>xList = new ArrayList<>();
+        while(x!=0){
+            //一桁ずつListに入れる
+            xList.add(x % 10);
+            x /=10;
+        }
+        while(xList.size()>1){
+            if(xList.get(0) != xList.get(xList.size()-1)) return false;
+            xList.remove(0);
+            xList.remove(xList.size()-2);
+        }
+        return true;
+    }
 }
