e06786e1da7b6ba178879cd34cbb2f6e129a46a0
qwertytoki <qwertytoki@gmail.com>
2019-04-12 12:14:05 +0800

    first commit

---
+++ Maximum Depth of Binary Tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        // 深さを探る
+        // 自分の深さをもって潜ればいいのかな？
+        // rootとdepthをもって、潜る
+        // 最深部まできたら、その値返す
+        // 返された値がmaxDepthより深ければ、maxDepthを入れ替える
+        if(root == null){
+            return 0;
+        }
+        return dive(root,0);
+        
+    }
+    private int dive(TreeNode root,int depth){
+        if(root.right== null&&root.left==null){
+            return depth;
+        }
+        depth++;
+        int right = dive(root.right,depth);
+        int left = dive(root.left,depth);
+        return right>=left ? right:left;
+    }
+}
