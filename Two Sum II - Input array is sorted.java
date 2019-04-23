0f75c4d27076859ea4fe92cf23df0be2b6c3fc5b
qwertytoki <qwertytoki@gmail.com>
2019-04-24 01:57:27 +0800

    first solution

--- Two Sum II - Input array is sorted.java
+++ Two Sum II - Input array is sorted.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        if(target<=numbers[0]){
+        if(target<=numbers[0]){
             return new int[]{};
         }
         int[] result = new int[2];
         int targetCandi = 0;
         for(int i = 0;i<numbers.length;i++){
             targetCandi = target - numbers[i];
-            if(numbers.contains(targetCandi)){
+            if(Arrays.asList(numbers).contains(targetCandi)){
                 result[0] = i+1;
                 for (int j=i+1;j<numbers.length;j++){
                     if(j==targetCandi){
-                        return new int[]{i+1,j;1};
+                        return new int[]{i+1,j+1};
                     }
                 }
             }
