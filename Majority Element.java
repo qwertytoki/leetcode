9175b865642b722aeffa2427f4bf8cc69574ee9b
qwertytoki <qwertytoki@gmail.com>
2019-04-25 02:04:03 +0800

    pass

--- Majority Element.java
+++ Majority Element.java
@@ -1,14 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
         // マジョリティ探し
-        Map<Integer,Integer> countMap = new HashMap()<>;
+        if(nums.length==1){
+            return nums[0];
+        }
+        Map<Integer,Integer> countMap = new HashMap<>();
         for(int num:nums){
-            if(countMap.contains(num)){
-                int a =countMap.get(num)++;
+            if(countMap.containsKey(num)){
+                int a = countMap.get(num);
+                a++;
                 if(a>nums.length/2){
-                    return a;
+                    return num;
                 }
-                countMap.put(a);
+                countMap.put(num,a);
             }else{
                 countMap.put(num,1);
             }
