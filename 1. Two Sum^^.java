fac59da66cbbe908aaaa4f03b20bbda778760337
qwertytoki <qwertytoki@gmail.com>
2019-08-07 19:26:57 +0800

    1

--- 1. Two Sum^^.java
+++ 1. Two Sum^^.java
@@ -1,12 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer>map = new HashMap<>();
         for(int i =0;i<nums.length;i++){
-            for(int j=0;j<nums.length;j++){
-                if(i==j)continue;
-                if(nums[j]== target-nums[i]){
-                    return new int[]{i,j};
-                }
+            if(map.get(nums[i])!= null){
+                return new int[]{map.get(nums[i]),i};
             }
+            map.put(target-nums[i],i);
         }
         throw new IllegalArgumentException("no matching result");
     }
