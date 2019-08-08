ca997a1a867d1dfe18292af511399284fd3ebbfd
qwertytoki <qwertytoki@gmail.com>
2019-08-08 17:05:21 +0800

    done

---
+++ codility2_OddOccurrencesInArray.java
@@ -0,0 +1,24 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+import java.util.*;
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        Arrays.sort(A);
+        // for(int i :A){
+        //     System.out.println(i);    
+        // }
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i :A){
+            if(stack.peek()==null||stack.peek()!=i){
+                stack.add(i);
+            }else{
+                stack.pop();
+            }
+        }
+        return stack.peek();
+    }
+}
