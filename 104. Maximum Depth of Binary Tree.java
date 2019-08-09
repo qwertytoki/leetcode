83ba25ec1df21e9f725f325a6cd9a8e8db4520b9
qwertytoki <qwertytoki@gmail.com>
2019-08-09 23:51:03 +0800

    104 wip

---
+++ 104. Maximum Depth of Binary Tree.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root = null)return 0;
+        
+        int depth = 0;
+        
+        if(root != null)
+        
+        System.out.println(root.val);
+        System.out.println(root.right.val);
+        return depth;
+    }
+}
