3ff2ea7a99f92c1f86c4ad814d0d00ced2f9d319
qwertytoki <qwertytoki@gmail.com>
2019-08-10 18:15:13 +0800

    again

--- 703. Kth Largest Element in a Stream^^.java
+++ 703. Kth Largest Element in a Stream^^.java
@@ -3,23 +3,25 @@ import java.util.PriorityQueue;
 
 class KthLargest {
     final PriorityQueue<Integer> pq = new PriorityQueue<>();
+    int kth =0;
+
     public KthLargest(int k, int[] nums) {
-        
-        for(int i :nums){
+        kth =k;
+
+        for (int i : nums) {
             pq.add(i);
         }
-        while(pq.size() > k){
+        while (pq.size() > k) {
             pq.poll();
-         }
+        }
     }
-    
+
     public int add(int val) {
-        if(pq.size()==0){}
+        if (kth > pq.size()) {
             pq.add(val);
-    }
-        if(val>pq.peek()){
-           pq.add(val);
-           pq.poll(); 
+        } else if (val > pq.peek()) {
+            pq.add(val);
+            pq.poll();
         }
         return pq.peek();
     }
