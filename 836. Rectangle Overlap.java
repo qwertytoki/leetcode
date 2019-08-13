95402863484f18583a8f436d5253dd1613976f03
qwertytoki <qwertytoki@gmail.com>
2019-08-14 00:03:57 +0800

    1st commit

--- 836. Rectangle Overlap.java
+++ 836. Rectangle Overlap.java
@@ -10,13 +10,22 @@ class Solution {
         int r2x2 = rec2[2];
         int r2y2 = rec2[3];
         
-        // condition1 
+        // condition1 x,y included
         if((r1x1<r2x1 && r2x1<r1x2 &&r1y1<r2y1 && r2y1<r1y2)
             ||(r1x1<r2x2 && r2x2<r1x2 &&r1y1<r2y2 && r2y2<r1y2)){
                 return true;                
         }
-        // condition2 
+        // condition2 x same, y included or y same, x included
+        if((r1x1==r2x1 && r1y1<r2y1 && r2y1<r1y2)
+        ||(r1y1==r2y1 && r1x1<r2x1 && r2x1<r1x2)){
+            return true;
+        }
         
+        // condition3 all point matched
+        if(r1x1==r2x1 &&r1y1==r2y1 &&r1x2==r2x2 &&r1y2==r2y2){
+            return true;
+        }
+
         return false;
 
     }
